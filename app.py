230a23ad3f38617b1ebb551fb206711d21a4f515
SayantanPal9 <sayantanpal2006.24@kgpian.iitkgp.ac.in>
2026-02-14 22:08:10 +0530

    jj

---
+++ app.py
@@ -0,0 +1,16 @@
+import streamlit as st
+import openoa
+import pandas as pd
+
+st.title("OpenOA Deployment Demo")
+
+st.write("Version:", openoa.__version__)
+
+# simulate backend usage
+data = pd.DataFrame({
+    "Wind Speed": [5, 7, 9],
+    "Power": [100, 200, 350]
+})
+
+st.write("Sample wind data:")
+st.dataframe(data)
